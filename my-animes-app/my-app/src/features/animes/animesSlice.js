aa40af3f3e0189b5758ee26f02941966c33f6431
ayoubbenaissa <ayoub.benissa@supcom.tn>
2022-02-27 22:44:54 +0100

    my-animes-app: prepared getAllAnimes thunk

--- my-animes-app/my-app/src/features/animes/animesSlice.js
+++ my-animes-app/my-app/src/features/animes/animesSlice.js
@@ -1,4 +1,47 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-import { getAllAnimes, getAnime, updateAnime, deleteAnime } from './animesAPI';
+import { getAllAnimes, getAnime, updateAnime, deleteAnime } from "./animesAPI";
 
+const initialState = {
+  allAnimes: [],
+  currentAnime: {},
+  allAnimesReady: "loading",
+  currentAnimeReady: "loading",
+  error: null,
+};
+
+export const getAllAnimesAction = createAsyncThunk(
+  "animes/getAllAnimes",
+  async (_, reject) => {
+    try {
+      const allAnimes = await getAllAnimes(),
+        allAnimesData = allAnimes.json();
+
+      return allAnimesData;
+    } catch (err) {
+      reject(err.message);
+    }
+  }
+);
+
+export const animesSlice = createSlice({
+  name: "animes",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllAnimesAction.pending, (state) => {
+        state.allAnimesReady = "loading";
+      })
+      .addCase(getAllAnimesAction.fulfilled, (state, action) => {
+        state.allAnimesReady = "fulfilled";
+        state.allAnimes = action.payload;
+        state.error = null;
+      })
+      .addCase(getAllAnimesAction.rejected, (state, action) => {
+        state.allAnimesReady = "rejected";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectAllAnimes = (state) => state.animes.allAnimes;
